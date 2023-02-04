0d258a77fd439175a7346752f9fa3f168eb433a2
Sathyamurthy Kamath <44369630+Sathyamurthyskamat@users.noreply.github.com>
2023-02-04 16:07:09 +0530

    Update dht.py

--- dht.py
+++ dht.py
@@ -1,10 +1,18 @@
 import sys
 import Adafruit_DHT
 import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("file.json")
+firebase_admin.initialize_app(cred,{'https://rasp-f69dc-default-rtdb.firebaseio.com/'},None)
 
 while True:
-
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    users_ref = ref.child('Data')
+    users_ref.set({"data": {
+        'Temperature': 'Temp: {0:0.1f} C'.format(temperature),
+        'Humidity': 'Humidity: {1:0.1f} %'.format(humidity)} })
+    #print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     time.sleep(1)
